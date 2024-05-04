c1935b9d821395f925dde9de35abd5236e970ed8
ElisabettaPautasso <s297027@studenti.polito.it>
2024-05-04 18:44:32 +0200

    idea calcolo intersezione per frattura 0 e 2

--- Project/main.cpp
+++ Project/main.cpp
@@ -2,12 +2,16 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include <Eigen/Dense>
+#include <Eigen/Eigen>
 #include "Traccia.hpp"
 #include "Utils.hpp"
 
 using namespace std;
+using namespace Eigen;
 using vec3 = vector<vector<vector<double>>>;
 
+<<<<<<< Updated upstream
 // Funzione che controlla che due fratture si intersechino e calcola i punti della traccia esistente
 // Fracture è il nome dell'oggetto che contiene tutte le fratture.
 void Find_Traces(Fracture& frac)
@@ -131,6 +135,34 @@ struct Traces
     map<int,bool> passing_traces; // id_traccia tips 1/0
 
 };
+=======
+vector<double> vettore(vec3 fracture, vector <double> v, unsigned int i, unsigned int j) // i indica quale frattura considero
+{
+    v = { fracture[i][j][0] - fracture[i][j-1][0],
+          fracture[i][j][1] - fracture[i][j-1][1],
+          fracture[i][j][2] - fracture[i][j-1][2] };
+
+    return v;
+}
+
+vector<double> crossProduct(const vector<double>& u, const vector<double>& v) {
+    vector<double> w(3);
+    w[0] = u[1] * v[2] - u[2] * v[1];
+    w[1] = u[2] * v[0] - u[0] * v[2];
+    w[2] = u[0] * v[1] - u[1] * v[0];
+    return w;
+}
+
+double dotProduct(const vector<double>& v1, const vector<double>& v2)
+{
+    double ris = 0;
+    for (int i = 0; i < 3; ++i) {
+        ris += v1[i] * v2[i];
+    }
+    return -ris;
+}
+
+>>>>>>> Stashed changes
 
 int main()
 {
@@ -142,6 +174,91 @@ int main()
     }
     else
         cout << "Import effettuato con successo" << endl;
+
+    // vediamo come calcolare i punti di intersezione tra due poligoni
+    // scelgo frattura 0 e frattura 2
+
+    vector <double> AB(3);
+    vector <double> BC(3);
+    vector <double> LM(3);
+    vector <double> MN(3);
+
+    AB = vettore(fracture, AB, 0, 1);
+    BC = vettore(fracture, BC, 0, 2);
+    LM = vettore(fracture, LM, 2, 1);
+    MN = vettore(fracture, MN, 2, 2);
+
+    vector <double> n1 = crossProduct(AB, BC);
+    vector <double> n2 = crossProduct(LM, MN);
+
+
+
+    // piano per frattura 0 identificato da (a,b,c,d)
+    double d1 = dotProduct(n1,fracture[0][0]);
+    vector <double> piano0 = n1;
+    piano0.push_back(d1);
+
+    // piano per frattura 2 identificato da (a,b,c,d)
+    double d2 = dotProduct(n2,fracture[2][0]);
+    vector <double> piano2 = n2;
+    piano2.push_back(d2);
+
+    // retta per AD, identificata dal punto A e il vettore AD
+    vector <double> AD = { fracture[0][3][0] - fracture[0][0][0],
+                         fracture[0][3][1] - fracture[0][0][1],
+                         fracture[0][3][2] - fracture[0][0][2] };
+
+    // converto la forma parametrica di AD in forma cartesiana, quindi trovo i due piani nella forma di prima
+    // x = 0, z = 0 è la forma cartesiana di questa retta
+    vector <double> pi1(4);
+    vector <double> pi2(4);
+    vector <double> pi3(4);
+
+
+    pi1[0] = AD[1];
+    pi1[1] = -AD[0];
+    pi1[3] = AD[0]*fracture[0][0][1] - AD[1]*fracture[0][0][0];
+
+
+    pi2[0] = AD[2];
+    pi2[2] = -AD[0];
+    pi2[3] = AD[0]*fracture[0][0][2] - AD[2]*fracture[0][0][0];
+
+    pi3[1] = AD[2];
+    pi3[2] = -AD[1];
+    pi3[3] = AD[1]*fracture[0][0][2] - AD[2]*fracture[0][0][1];
+
+    // ora scivo la matrice dei coefficienti fatta da piano0, piano 2, pi1, pi3 e vettore delle incognite (x,y,z,1),
+    // l'intersezione sarà (x,y,z)
+
+    Matrix<double, 4, 3> coeff;
+    coeff.row(0) << piano0[0], piano0[1], piano0[2];
+    coeff.row(1) << piano2[0], piano2[1], piano2[2];
+    coeff.row(2) << pi1[0], pi1[1], pi1[2];
+    coeff.row(3) << pi3[0], pi3[1], pi3[2];
+
+    Vector4d termineNoto;
+    termineNoto[0] = piano0[3];
+    termineNoto[1] = piano2[3];
+    termineNoto[2] = pi1[3];
+    termineNoto[3] = pi3[3];
+
+    HouseholderQR<Eigen::Matrix<double, 4, 3>> qr(coeff);
+    Vector3d soluzione = qr.solve(termineNoto);
+    for(unsigned int i = 0; i < 3; i++)
+        cout << soluzione[i] << " ";
+    cout << endl;
+
+
+
+
+
+
+
+
+
+
+
 
     return 0;
 
